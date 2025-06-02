0e5665d15826cb677aeba9c21171755709959cc3
eshika17 <changuleankita@gmail.com>
2025-06-02 10:10:40 +0530

    Add files via upload

---
+++ abc/assign1.c
@@ -0,0 +1,21 @@
+
+ #include <stdio.h>
+
+int main() {
+int num1, num2, sum;
+
+printf("Enter first integer: ");
+scanf( "%d", &num1);
+    
+printf("Enter second integer: ");
+scanf("%d" , &num2);
+    
+sum = num1 + num2;
+
+printf("The sum of %d and %d is %d", num1, num2, sum);
+
+    
+}
+
+	
+
---
+++ abc/assign6.c
@@ -0,0 +1,16 @@
+int main()
+ {
+    int num;
+    
+    int square,cube;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    square = num * num;
+    cube = num * num * num;
+    
+     printf("Square of %d is %d\n", num, square);
+    printf("Cube of %d is %d\n", num, cube);
+    
+}
+
