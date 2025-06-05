2bb0c80a814ec8c2fc693ac7c775e19b7083452a
eshika17 <changuleankita@gmail.com>
2025-06-05 12:24:07 +0530

    Add files via upload

---
+++ A.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int hr,min,sec;
+	printf("enter hours minutes and seconf");
+	scanf("%d%d%d",&hr,&min,&sec);
+	int time=hr*3600*60*30;
+	
+	printf("the total second are %d:time");
+}  
