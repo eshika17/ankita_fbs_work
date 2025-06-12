b9d57759f27708117273c884ac48c6cd950fffc4
eshika17 <changuleankita@gmail.com>
2025-06-12 20:09:40 +0530

    Add files via upload

---
+++ cprogram/assignment1/assignment5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+ {
+    int age=2;
+    if (age >= 18) 
+	{
+        printf("You are eligible to vote.\n");
+    } 
+	else
+	{
+        printf("You are not eligible to vote.\n");
+    }
+}
---
+++ cprogram/assignment1/assignment6.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+ {
+    char ch='C';
+   
+    if(ch>='A'&&ch<='Z')
+	 {
+        printf("The character c is uppercase",ch);
+    }
+    
+    else if(ch>='A'&&ch<='Z')
+	 {
+        printf("The character c is lowercase",ch);
+    
+   
+    }
+
+   
+}
