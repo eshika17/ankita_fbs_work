a30f33a8e2c7b367e6840992a6c18f8d59ffc2d7
eshika17 <changuleankita@gmail.com>
2025-06-19 15:25:54 +0530

    Add files via upload

---
+++ cprogram/Class Test/Test2/a2.c
@@ -0,0 +1,14 @@
+//wap to check if number is positive,nigative or neutral
+void main()
+{
+	int n;
+	printf("enter the number");
+	scanf("%d",&n);
+
+	if(n>0)
+	printf("%d is positive",n);
+	else if(n<0)
+	printf("%d is nigative",n);
+	else 
+	printf("%d is neutral",n);
+}
